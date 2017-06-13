fba848c59c02b1a416a3014b97106cbece42f17e
satoshy <satoshy@ya.ru>
2017-06-13 23:36:28 +0300

    smarty images and header

--- app/themes/smarty/assets/javascripts/smarty/all.js
+++ app/themes/smarty/assets/javascripts/smarty/all.js
@@ -4,4 +4,10 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//= require jquery
+//= require jquery_ujs
+//= require jquery.tokeninput
+//= require best_in_place
+//= require bootstrap
+//= require turbolinks
 //= require_tree .
